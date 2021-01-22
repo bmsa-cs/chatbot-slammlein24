4f80f6d8bc6e4770e9af361b7a7b02abfd6ea83e
Sage Lammlein <slammlein24@biomedscienceacademy.org>
2021-01-22 19:44:17 +0000

    I added comments to my code and fixed a few typos

--- main.py
+++ main.py
@@ -25,12 +25,13 @@ def run_tests():
 
 def main():
   """This function contains all code for the chatbot."""
-
+#This is the introduction to the code. It asks the user for their name in order to get to know their user.
   print("Hello!")
   name = input("What is your first name? ")
   lastname = input("What is your last name? ")
   print("Hello, "+name+" "+lastname+"! I'm glad I have met you.")
 
+#This part of the code asks the user how they are feeling today. Based on their answer, it will result in different things.
   feeling = input("Okay, "+name+", how is your day so far? ")
   if (feeling == "good"):
     print("I'm glad your day has been good. It makes me feel happy.")
@@ -41,17 +42,20 @@ def main():
     bad_day = input("What made your day bad? ")
     print("Well I hope that everything gets better soon.")
 
+#This part of the code asks the user about their favorite color. If they end up liking the color green or black or yellow, then the code will say that they have the same favorite color.
   color = input("So tell me, "+name+", what is your favorite color? ")
-  if (color == "green" or "black"):
+  if (color == "green" or "black" or "yellow"):
     print("That's my favorite color too!")
   else:
     print("That's cool.")
 
+#This section of the code asks the user about a nickname. If the user says they have one, it will ask them what the nickname is.
   nickname = input("I'm curious, "+name+". Do you have a nickname? ")
   if (nickname == "yes"):
     what_nickname = input("What is it? ")
     print("Wow. I like that nickname.")
   
+  #This large section of code asks the user what their favorite season is. Based on their input will result in a different response.
   season = input("What is your favorite season, "+name+"? ")
   if(season == "winter"):
     print("I love how the trees look when it snows. Winter is definitely a pretty season.")
@@ -62,12 +66,14 @@ def main():
   elif(season == "summer"):
     print("I should have guessed. Summer is a very relaxing season and it is pretty warm too.")
   
+  #This part of the code asks the user what their favorite food is. If they say their favorite food is tacos, then it will print "Wow, I love tacos too!"
   food = input("Say, "+name+". What is your favorite food? ")
   if(food == "tacos"):
     print("Wow, I love tacos too!")
   else:
     print("That sounds tasty!")
   
+  #This code asks the user what their favorite flower is. The computer will then say its favorite flower based on a random number.
   flower = input("Okay last question, "+name+". What is your favorite flower? ")
   computer_flower = random.randint(1,3)  
   print("Wow that's a pretty flower.")
@@ -77,6 +83,7 @@ def main():
     print("My favorite flower is Hibiscus.")
   else:
     print("My favorite flower is Frangipani.")
+
   print("Well it was nice talking to you, "+name+".")
   
 
